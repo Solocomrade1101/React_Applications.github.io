f120e9b6f34ceade4f43ca16d6a088f6eda10f1c
Solocomrade1101 <solocomrade@mail.ru>
2023-07-06 20:07:34 +0300

    исправил путь home страницы.

--- src/components/Navigations/Navigation.jsx
+++ src/components/Navigations/Navigation.jsx
@@ -5,10 +5,10 @@ import { NavLink } from 'react-router-dom';
 function Navigation(props) {
   return (
     <div4 className={nav.block}>
-      <div className={nav.link} ><NavLink to="/" className={nav.nav_link} activeClassName="active" >Змейка</NavLink></div>
-      <di1 className={nav.link} ><NavLink to="/calculator"  className={nav.nav_link} activeClassName="active" >Калькулятор</NavLink></di1>
-      <di2 className={nav.link} ><NavLink to="/casino"  className={nav.nav_link} activeClassName="active" >Казино</NavLink></di2>
-      <div className={nav.link} ><NavLink to="/tictactoe" className={nav.nav_link}  activeClassName={nav.active} >Крестики-нолики</NavLink></div>
+      <div className={nav.link} ><NavLink exact to="" className={nav.nav_link} activeClassName="active" >Змейка</NavLink></div>
+      <di1 className={nav.link} ><NavLink to="/calculator" className={nav.nav_link} activeClassName="active" >Калькулятор</NavLink></di1>
+      <di2 className={nav.link} ><NavLink to="/casino" className={nav.nav_link} activeClassName="active" >Казино</NavLink></di2>
+      <div className={nav.link} ><NavLink to="/tictactoe" className={nav.nav_link} activeClassName={nav.active} >Крестики-нолики</NavLink></div>
     </div4>
   );
 }
